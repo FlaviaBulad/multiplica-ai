58f82f29bfc55e94c0b7f84a568e3f8ecada12e0
Flavia <flavia.bulad@gmail.com>
2023-01-10 14:57:17 -0300

    switch language to pt-br

--- src/App.java
+++ src/App.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Enter a number:");
+        System.out.println("Digite um número");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         input.close();
-        System.out.println("Multiplication table of " + number + " is:");
+        System.out.println("Tabela de multiplicação de " + number);
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " x " + i + " = " + number * i);
         }
