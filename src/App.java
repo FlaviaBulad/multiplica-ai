1480fc611d451cca3bc17961e6a7c39f67c9bc08
Flavia <flavia.bulad@gmail.com>
2023-01-10 14:31:59 -0300

    add solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Enter a number:");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        input.close();
+        System.out.println("Multiplication table of " + number + " is:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + number * i);
+        }
     }
 }
